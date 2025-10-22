a8a6d2e6d23463f2b9323b5594f3c65168a3d3e9
Gonzalo Ramos <gonzaloalexramos@gmail.com>
2025-10-22 08:36:50 -0300

    .

--- CivitaBack/Program.cs
+++ CivitaBack/Program.cs
@@ -112,7 +112,7 @@ using (var scope = app.Services.CreateScope())
     context.Database.Migrate(); // Aplica solo las migraciones pendientes
 }
 
-// Pipeline
+// Pipeline 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
