b2e83cb15e5106a6c411c4a01418540fa5fb4286
Gonzalo Ramos <gonzaloalexramos@gmail.com>
2025-11-16 20:33:25 -0300

    avance

---
+++ CivitaBack.Logica/NivelLogica.cs
@@ -0,0 +1,19 @@
+﻿using CivitaBack.Domain.Entidades;
+
+namespace CivitaBack.Logica;
+
+public interface INivelLogica
+{
+    Task SaberSiPuedeSubirNivel(Recurso recursos);
+
+}
+
+public class NivelLogica : INivelLogica
+{
+    public Task SaberSiPuedeSubirNivel(Recurso recursos)
+    {
+        throw new NotImplementedException();
+    }
+    
+    private void 
+}
